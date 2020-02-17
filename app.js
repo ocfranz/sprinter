c5962df694d1c62aa8792fe170a0df25ad4430e4
Franz Ogosi Cahuana <franz.ogosi@usil.pe>
2020-02-17 18:25:44 -0500

    Moongose conecction

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const http = require("http");
 const path = require("path");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
+const usersRoute = require('./routes/users');
 
 dotenv.config();
 
@@ -13,9 +14,7 @@ app.use(express.static(__dirname + "/assets"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "src", "index.html"));
-});
+app.use('/users', usersRoute);
 
 app.listen(process.env.PORT, () =>
   console.log(`Server is listening on port ${process.env.PORT}`)
--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,8 @@
 const express = require('express');
-const router = express.Router();
-const config = require('../config.js');
+const mongoose = require('mongoose');
 const path = require('path');
+const router = express.Router();
+const config = require('../config/config');
 
 mongoose.connect(
   config.connection,
